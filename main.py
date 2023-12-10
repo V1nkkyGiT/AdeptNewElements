0d7389f33d00c5f95ee3d13c7e9ed331d971c20d
V1nkkyGiT <152776712+V1nkkyGiT@users.noreply.github.com>
2023-12-10 07:17:10 +0000

    BABAH

---
+++ main.py
@@ -0,0 +1,15 @@
+from character import Character
+
+player1 = Character('Krutoy')
+player2 = Character('Bob', damage=5)
+print(player1, end='\n\n')
+print(player2)
+print()
+
+p1_damage = player1.attack(player2)
+print(f'{player1.name} атакував '
+      f'{player2.name} і наніс '
+      f'{p1_damage} шкоди')
+print()
+print(player1, end='\n\n')
+print(player2)
